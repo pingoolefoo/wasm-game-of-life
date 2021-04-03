c92762997c1f2b2972743e4a1904c45fa9435500
PingooLeFoo <charles.casadei.pro@gmail.com>
2021-04-03 08:57:48 +0200

    02 rendering from memory

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,18 @@ pub struct Universe {
 /// Public methods, exported to JavaScript
 #[wasm_bindgen]
 impl Universe {
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.width
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
     pub fn tick(&mut self) {
         let mut next = self.cells.clone();
 
